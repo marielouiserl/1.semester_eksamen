5f7d1cbade55424cf92147fcc6f4e0cdf6ac225b
Marie-Louise Rasch Larsen <Marie-Louise@Maries-MacBook-Pro-3.local>
2019-06-12 15:18:07 +0200

    javascript tilføjet i splash

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-window.addEventListener("load", bounceArrow);
+var i = 0;
+var txt = 'THIS IS A PORTFOLIO';
+var speed = 150;
 
-function sidenVises() {
-    console.log("sidenVises");
-
-
-
-}
+window.addEventListener("load", typeWriter);
 
 
-function bounceArrow() {
-    console.log("bouncing");
 
 
+function typeWriter() {
+    console.log("typing");
+
+    if (i < txt.length) {
+        document.getElementById("animation1").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
 }
